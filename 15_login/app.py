eb3ace81955d1920423b73f2e71d4c3c5375ed41
Nahi.k <47545708+NahiKhan@users.noreply.github.com>
2019-10-04 13:48:47 -0400

    Update app.py

--- 15_login/app.py
+++ 15_login/app.py
@@ -8,27 +8,70 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import session
+import os
 
 app = Flask(__name__)
 
+# hard-coded login credentials
+user = "dirtydashers"
+pswd = "password99"
+
+# creating secret key for session (staying logged in)
+app.secret_key = os.urandom(32)
+
+# preparing a string variable for outputting error messages
+errorMsg = ""
+
 @app.route('/')
 def root():
-    return render_template("login.html")
+    # checking to see if the user has already logged in
+    if 'user' in session:
+        return redirect(url_for('welcome'))
+    return redirect(url_for('login'))
 
 @app.route('/welcome')
 def welcome():
     return render_template("welcome.html")
 
-@app.route('/error')
-def error():
-    return render_template("error.html")
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
+# handles the login process
 @app.route("/auth")
 def auth():
-    if request.args["username"] == "qbert" and request.args["password"] == "ahmednahi":
-        return redirect(url_for('welcome'))
+    inputUser = request.args['username']
+    inputPswd = request.args['password']
+    # checking to see if input credentials match hard coded credentials
+    if inputUser == user:
+        if inputPswd == pswd:
+            session['user'] = inputUser
+            return redirect(url_for('welcome'))
+        else:
+            errorMsg = "[INVALID PASSWORD]"
+            return error(errorMsg)
     else:
-        return redirect(url_for('error'))
+        if inputPswd != pswd:
+            errorMsg = "[INVALID USERNAME and PASSWORD]"
+            return error(errorMsg)
+        else:
+            errorMsg = "[INVALID USERNAME]"
+            return error(errorMsg)
+
+# for some reason, creating an error route does not include the message
+# thus, we decided to remove it and instead just return a customized message in an error template
+def error(errorMsg):
+    return render_template(
+        "error.html",
+        error=errorMsg
+    )
+
+# logout process only accessible from the logged-in page
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect(url_for('root'))
 
 if __name__ == "__main__":
     app.debug = True
